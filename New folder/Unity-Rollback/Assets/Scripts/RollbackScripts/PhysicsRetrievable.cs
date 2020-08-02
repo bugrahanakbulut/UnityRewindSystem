f93b9395fdfbc0a66b71a461eb3563f3f3721cbd
bugra <bugra@mildmania.com>
2020-08-02 17:17:58 +0300

    GitIgnore

---
+++ New folder/Unity-Rollback/Assets/Scripts/RollbackScripts/PhysicsRetrievable.cs
@@ -0,0 +1,41 @@
+﻿using RollbackSys;
+using UnityEngine;
+
+namespace GunSys
+{
+    public class PhysicRetrievableTimeStamp : RetrievableTimeStampBase
+    {
+        public Vector3 Position { get; }
+        
+        public Quaternion Rotation { get; }
+        
+        public Vector3 Velocity { get; }
+
+        public PhysicRetrievableTimeStamp(Vector3 position, Quaternion rotation, Vector3 velocity)
+        {
+            Position = position;
+            Rotation = rotation;
+            Velocity = velocity;
+        }
+    }
+    
+    public class PhysicsRetrievable : RetrievableBase<PhysicRetrievableTimeStamp>
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+        
+        protected override bool ExecuteTimeStamp(PhysicRetrievableTimeStamp timeStamp)
+        {
+            transform.position = timeStamp.Position;
+            transform.rotation = timeStamp.Rotation;
+
+            _rigidbody.velocity = timeStamp.Velocity;
+            
+            return true;
+        }
+
+        protected override PhysicRetrievableTimeStamp GetTimeStamp()
+        {
+            return new PhysicRetrievableTimeStamp(transform.position, transform.rotation, _rigidbody.velocity);
+        }
+    }
+}
